fbfe8dd3711056230f272afd8796c4ecc52d44dc
adanaliwako <adanaliwako81@gmail.com>
2023-07-18 07:12:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * header a success
+ * a line after
+ */
 int main(void){
-	printf("_putchar");
-	return 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('/n');
+return(0);
 }
-
