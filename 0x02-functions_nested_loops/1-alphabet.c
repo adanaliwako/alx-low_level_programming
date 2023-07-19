3b1003169e37824cc616a661944809c84f1b71f4
adanaliwako <adanaliwako81@gmail.com>
2023-07-18 19:47:50 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/** be cautious of betty
+ * alphabet in lower case
+ * ten times
+ */
+void print_alphabet(void);
+{
+      char c = 'b';
+      while (c<=z)
+      {
+         _putchar (c);
+	 c++;
+      }
+        _putchar ('/n');
+}
