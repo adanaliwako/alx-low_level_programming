53b45e707dd3f01ce5bb486a46dbfc1e664c56ae
adanaliwako <adanaliwako81@gmail.com>
2023-07-19 19:32:23 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _abs(int);
+{         
+	  do
+	  {
+          int a = 0;
+	  int b;
+	  int c = b-a;
+	  }
+	  while(b > 0)
+	  _putchar('c');
+
+}
