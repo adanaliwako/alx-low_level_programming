989006a8a37f992fa445c032cf9342f4e051b7ca
adanaliwako <adanaliwako81@gmail.com>
2023-07-21 13:40:04 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	char c;
+	
+	if(c <= 'Z')
+	{
+		printf("%C" is uppercase);
+	}
+
+	else if (c <=z )
+	{
+		printf("%c" is lowercase);
+		return (0);
+	}
+}
